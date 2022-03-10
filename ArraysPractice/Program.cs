9f0cb7ceff33d6fef9c39710a95f2e302ca33d56
Allison Anguiano <aanguiano@dev-10.com>
2022-03-10 15:21:13 -0600

    Indexing the 2D array

--- ArraysPractice/Program.cs
+++ ArraysPractice/Program.cs
@@ -13,28 +13,10 @@ namespace ArraysPractice
                 { "Atlantic", "Pacific", "Indian" }
             };
 
-            Console.WriteLine($"{threeOfAKind}");       //how can I see this other than by indexing?
+            string element = threeOfAKind[1, 2];
 
-            //3D Int array
-            int[,,] valueCube = {
-            {
-                { 1, 2, 3 },
-                { 4, 5, 6 },
-                { 7, 8, 9 }
-            },
-            {
-                { 11, 12, 13 },
-                { 14, 15, 16 },
-                { 17, 18, 19 }
-            },
-            {
-                { 21, 22, 23 },
-                { 24, 25, 26 },
-                { 27, 28, 29 }
-            }
-            };
-
-            Console.WriteLine($"{valueCube}");
+            Console.WriteLine(element);            // Blue
+            Console.WriteLine(threeOfAKind[2, 0]); // Atlantic
 
         }
     }
